271ce2c18b705510b1ac822046ff53fed4d45133
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-08-14 16:12:01 -0500

    working on layout

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(express.json);
+app.set('view engine' , 'ejs');
+
+// middleware incorporated
+
+
+// routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`The app is listening on port ${port}!`);
+    console.log(`To access the website, go to http://localhost:${port}`);
+})

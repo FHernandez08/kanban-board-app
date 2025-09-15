74f42a89ca4f2f894f1c8827c07232a5ebbc486a
FHernandez08 <fidelhernandez_8@hotmail.com>
2025-09-15 16:56:32 -0500

    working on drag-drop feature - pointermove

--- public/js/drag-and-drop.js
+++ public/js/drag-and-drop.js
@@ -1,4 +1,4 @@
-const card = document.getElementById("card");
+const card = document.getElementById("test-card");
 const boardCell = document.getElementsByClassName("board-cell");
 
 // variables for when the drag event starts
@@ -8,15 +8,35 @@ let dragContext = null;
 
 card.addEventListener("pointerdown", (event) => {
     isDraggable = true;
+    console.log(isDraggable);
+    console.log("pointerdown fired");
 
     dragContext = {
         startX: event.clientX,
         startY: event.clientY,
-        rects: []
+        rects: [],
+        cardId: event.target,
+        previewEl,
     };
+
+    console.log(dragContext.startX, dragContext.startY);
 
     for (let cell of boardCell) {
         const rect = cell.getBoundingClientRect();
         dragContext.rects.push(rect);
+    }
+
+    console.log(dragContext.rects);
+});
+
+card.addEventListener("pointermove", (event) => {
+    if (!isDraggable || dragContext === null) {
+        return
+    }
+    else {
+        currentPositionX = event.clientX;
+        currentPositionY = event.clientY;
+
+        dragContext.previewEl.style.transform = translate(currentPositionX, currentPositionY);
     }
 })

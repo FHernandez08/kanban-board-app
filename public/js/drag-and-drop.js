2b9fba39719d46d841d9f6e572aa2656997b2069
Fidel Hernandez <fidelhernandez_8@hotmail.com>
2025-09-15 19:48:51 -0500

    drag and drop functionality - pointermove added

--- public/js/drag-and-drop.js
+++ public/js/drag-and-drop.js
@@ -11,13 +11,29 @@ card.addEventListener("pointerdown", (event) => {
     console.log(isDraggable);
     console.log("pointerdown fired");
 
+    const clone = event.target.cloneNode(true);
+    clone.classList.add("drag-preview");
+    clone.style.position = "fixed";
+    clone.style.pointerEvents = "none";
+    clone.style.opacity = "0.7";
+    clone.style.zIndex = "1000";
+
+    clone.style.left = event.clientX + "px";
+    clone.style.top = event.clientY + "px";
+
+    document.body.append(clone);
+
     dragContext = {
         startX: event.clientX,
         startY: event.clientY,
         rects: [],
         cardId: event.target,
-        previewEl,
+        previewEl: clone,
+        candidateCell: null,
+        prevCandidateCell: null
     };
+
+    event.target.setPointerCapture(event.pointerId);
 
     console.log(dragContext.startX, dragContext.startY);
 
@@ -30,13 +46,41 @@ card.addEventListener("pointerdown", (event) => {
 });
 
 card.addEventListener("pointermove", (event) => {
+    console.log(dragContext.previewEl)
     if (!isDraggable || dragContext === null) {
         return
     }
     else {
-        currentPositionX = event.clientX;
-        currentPositionY = event.clientY;
+        let x = event.clientX;
+        let y = event.clientY;
+        let candidateCell = null;
 
-        dragContext.previewEl.style.transform = translate(currentPositionX, currentPositionY);
+        let dx = x - dragContext.startX;
+        let dy = y - dragContext.startY;
+
+        dragContext.previewEl.style.transform = `translate(${dx}px, ${dy}px)`;
+
+        for (let entry of dragContext.rects) {
+            const rect = entry.rect;
+
+            if ((rect.left <= x && x <= rect.right) && (rect.top <= y && y <= rect.bottom)) {
+                candidateCell = entry.cell;
+                break;
+            }
+        }
+
+        dragContext.candidateCell = candidateCell;
+
+        if (candidateCell !== dragContext.prevCandidateCell) {
+            if (dragContext.prevCandidateCell) {
+                dragContext.prevCandidateCell.classList.remove('drop-hover');
+            }
+
+            if (candidateCell) {
+                candidateCell.classList.add('drop-hover');
+            }
+
+            dragContext.prevCandidateCell = candidateCell;
+        }
     }
 })
